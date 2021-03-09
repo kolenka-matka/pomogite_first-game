043be3b7bc43c96f1d53c33089a8073d961082c8
kolenka-matka <70626173+kolenka-matka@users.noreply.github.com>
2021-03-09 19:21:52 +0300

    Add files via upload

--- firt-game.py
+++ firt-game.py
@@ -1,14 +1,15 @@
 import pygame
+import random
 
 
-
-class Ball():
-    def _init_(self, pos, color):
-        self.pos = pos
-        self.color = color
+class Ball:
+    def __init__(self, pos):
+        self.pos = list(pos)
+        self.radius = random.randrange(15, 45)
+        self.color = (random.randrange(200, 255), random.randrange(30, 255), random.randrange(50, 255))
         self.right = True
         self.stop = False
-        pygame.draw.circle(screen, self.color, self.pos, 50)
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
 
     def horizontal_move(self):
         if not self.stop:
@@ -22,7 +23,8 @@ class Ball():
                 self.pos[0] -= 1
 
     def redraw(self):
-        pygame.draw.circle(screen, self.color, self.pos, 50)
+        pygame.draw.circle(screen, self.color, self.pos, self.radius)
+
 
 pygame.init()
 
@@ -32,34 +34,31 @@ x, y = 100, 100
 running = True
 right = True
 circle_color = (236, 176, 199)
-screen_color = (112, 146, 190)
+screen_color = (227, 218, 204)
 clock = pygame.time.Clock()
-FPS = 500
-stop = False # для кликов мышкой
+stop = False  # для кликов мышкой
+spisok = []
+FPS = random.randrange(200, 500)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # это буквально означает "если пользователь нажал на крестик"
             running = False  # в этом случае переменная running становится false и соответственно игра заканчивается
+        kol_balls = 1
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                stop = True
+                koor = event.pos  # координаты точки где мы тыкнули левой кнопкой мыши
+                ball = Ball(koor)
+                spisok.append(ball)
             else:
                 stop = False
-    if not stop:
-        if x == 550:
-            right = False
-        if x == 50:
-            right = True
-        if right:
-            x += 1
-        else:
-            x -= 1
     clock.tick(FPS)  # это нужно для того чтобы у тебя смена кадров происходила с какой-то скоростью fps
-    pygame.draw.circle(screen, circle_color, (x, y),
-                       50)  # (поверхность, на которой размещается фигура, цвет круга, (центр окружности), радиус)
+    for ball in spisok:
+        ball.horizontal_move()
+        ball.redraw()
+
     pygame.display.flip()  # это смена кадров
-    screen.fill((112, 146, 190))
+    screen.fill((235, 229, 221))
     clock.tick(FPS)
 
 pygame.quit()  # переменная отвечающая за время
